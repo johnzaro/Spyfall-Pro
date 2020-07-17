beacfc7240d02ac49a11683c63b9a9323673ee8b
johnzaro <johnzrgnns@gmail.com>
2020-07-17 14:22:41 +0300

    Initial Commit

---
+++ Spyfall Pro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Spyfall Pro
+//
+//  Created by John Zarogiannis on 17/7/20.
+//  Copyright © 2020 John Zarogiannis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
